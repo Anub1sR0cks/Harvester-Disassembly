c726b35d24bb94897d95ecdc3dd60b70e345cc3d
hsm <hsm@anubis.rocks>
2024-09-01 17:03:01 -0400

    Translated monotext.cpp.asm from x86 to C++

---
+++ CPP/SOURCE/monotext.cpp
@@ -0,0 +1,41 @@
+int monobox()
+{
+    return 4;
+}
+
+// Unused
+/*
+void log()
+{
+
+}
+*/
+
+// Unused
+/*
+void printf()
+{
+
+}
+*/
+
+// Unused
+/*
+void clear_line()
+{
+
+}
+*/
+
+// Unused
+/*
+void scroll_up()
+{
+
+}
+*/
+
+void put_character()
+{
+
+}
