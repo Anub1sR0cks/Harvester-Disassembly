b9d4a262532ed3752f7157d4fb232f064f42a141
hsm <hsm@anubis.rocks>
2024-09-01 19:19:08 -0400

    Translated chessman.cpp.asm from x86 to C++

---
+++ CPP/SOURCE/chessman.cpp
@@ -0,0 +1,39 @@
+#include "convutil.hpp"
+
+int talk_to_CHESSMASTER()
+{
+    // "I am the Chessmaster.  Any initiate who wishes to pass this way must do so over my dead body.  Being a civilized man, I offer my opponents a choice - solve my problem, or fight me to the death.  Do you know how to play chess?"
+    play_wav(16661, "CHESSMASTER", 0);
+
+    GetDlgFileString(34);
+
+    switch (get_response()) {
+        // 1. Yes!
+        case 1:
+            // "In that case, mate me and you may pass.""
+            play_wav(16674, "CHESSMASTER", 1);
+
+            if (chessgame() != 2) {
+                set_script_flag("CHESS_SOLVED", 1);
+
+                play_fvi("graphic\\fst\\c125.fst", 0);
+
+                KillNpc("CHESSMASTER", "SLASH");
+
+                return 0;
+            }
+
+        // 2. No...
+        case 2:
+            // "Chess is a civilized game.  How sad that we must now resort to a less civilized form of conflict."
+            play_wav(16678, "CHESSMASTER", 2);
+
+            execute_command("ADD_CHESS_KING_MONSTER");
+
+            play_fvi("graphic\\fst\\kinggrow.fst", 0);
+
+            break;
+    }
+
+    return 0;
+}
