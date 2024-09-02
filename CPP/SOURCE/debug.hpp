60e71d259e888720ccb798895d3f1f78ee8ac52b
hsm <hsm@anubis.rocks>
2024-09-02 14:24:27 -0400

    Translated 2 functions from main.cpp.asm
    - shut_down()
    - bogus()

--- CPP/SOURCE/debug.hpp
+++ CPP/SOURCE/debug.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
 extern char *log_buffer;
+
+void error_report(int id1, int id2, const char *message);
---
+++ CPP/SOURCE/main.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include "debug.hpp"
+
+int _argc;
+char **_argv;
+char **_envp;
+
+int main(int argc, char *argv[], char *envp[])
+{
+    _argc = argc;
+    _argv = argv;
+    _envp = envp;
+
+    return 0;
+}
+
+void initialize()
+{
+
+}
+
+void shut_down(int code)
+{
+    if (!XFILE_shutdown()) { error_report(0, 0x17c, "Could not uninitialize the extended file system."); }
+
+    // union REGS regs;
+    // regs.x.al = 0x03;
+    // int386(0x10, &regs, &regs);
+
+    exit(code);
+}
+
+void parse_command_line()
+{
+
+}
+
+void get_temp_directory()
+{
+
+}
+
+int verify_path(const char *path)
+{
+    return 0;
+}
+
+void bogus()
+{
+    puts("This bites!!!!!");
+}
